5ff6ad6a25ddb4266060f32b9ecd3206150e6ba3
Oleksandr Kovalchuk <anxolerd@outlook.com>
2021-06-12 11:12:47 +0300

    Update HelloExam.java
    Add current date to HelloExam messages

--- HelloExam.java
+++ HelloExam.java
@@ -1,5 +1,19 @@
-public class HelloExam {
-  public static void main(String[] args) {
-      System.out.println("Hello, exam");
+import java.util.Date;
+
+public class HelloExam{
+  private Date date;
+
+  public static void main(String[] args){
+    HelloExam greeter = new HelloExam();
+    greeter.greet();
+  }
+
+  HelloExam() {
+    this.date = new Date();
+  }
+
+  public void greet() {
+    System.out.println("Hello Exam!");
+    System.out.println("Today is " + this.date);
   }
 }
